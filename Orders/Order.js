aa0df75f0e379ea77dad3e642f16f086576d5cd2
ttrivedi96 <57979967+ttrivedi96@users.noreply.github.com>
2022-04-18 18:48:04 +0530

    Add files via upload

---
+++ Orders/Order.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+
+// User Model
+
+mongoose.model("Order", {
+    name: {
+		type: String,
+		required: true
+	},
+	customerId: {
+		type: mongoose.SchemaTypes.ObjectId,
+		required: true
+	},
+	amount: {
+		type: Number,
+		required: true
+	},
+	image: {
+		type: String,
+		required: true
+	},
+	createdAt: {
+		type: Date,
+		required: true
+	},
+	qty: {
+		type: Number,
+		required: false
+	}
+})
