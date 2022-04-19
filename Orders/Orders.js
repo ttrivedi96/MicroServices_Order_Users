780d1b0d8316935399db5c28293e6c779e9e5d77
ttrivedi96 <57979967+ttrivedi96@users.noreply.github.com>
2022-04-19 12:04:09 +0530

    Add files via upload

--- Orders/Orders.js
+++ Orders/Orders.js
@@ -11,14 +11,14 @@ app.use(bodyParser.json());
 const mongoose = require("mongoose");
 
 // Global User Object which will be the instance of MongoDB document
-var User = require('./Order');
+var Order = require('./Order');
 async function connectMongoose() {
 	await mongoose.connect('mongodb://localhost:27017/msUSer', { useNewUrlParser: true, useUnifiedTopology:true }).then(
     () =>{
 		console.log("mongoose connected..")
 	})
 	require("./Order")
-	User = mongoose.model("Order")
+	Order = mongoose.model("Order")
 }
 
 // Load initial modules
@@ -29,12 +29,7 @@ initialLoad()
 
 // GET all orders for a user
 // GET single order for a user
-/**
- * A single api can perform multiple operations based on the query params.
- * Also to improve this logic, we can create different util files or modules to handle the separate logic 
- * based on these query params.
- * As this is a straightforward simple condition, I have added them using if else condition here only.
- */
+
  app.get("/orders",async (req, res) => {
 	
 	if(!req.query.oid && req.query.uid) {
@@ -56,15 +51,16 @@ initialLoad()
 	}
 })
 
+
 // Create an order for a user
 app.post("/order", async (req, res) => {
 	const newOrder = {
-		"name":req.body.name,
-		"customerId":req.body.customerId,
-		"amount":req.body.amount,
-		"image":req.body.image,
-		"createdAt":req.body.createdAt,
-		"qty":req.body.qty,
+		"name": req.body.name,
+		"customerId": req.body.customerId,
+		"amount": req.body.amount,
+		"image": req.body.image,
+		"createdAt": req.body.createdAt,
+		"qty": req.body.qty,
 	}
 	
 	// Create new Order instance..
@@ -89,6 +85,9 @@ app.delete("/orders/:oid", async (req, res) => {
 	})
 })
 
+
+
+
 // Delete all orders for a user
 app.delete("/orders", async (req, res) => {
 	Order.findOneAndDelete({customerId: req.query.uid}).then((o) => {
@@ -99,6 +98,8 @@ app.delete("/orders", async (req, res) => {
 		}
 	})
 })
+
+
 
 // APP listening on port 5151
 app.listen(5151, () => {
